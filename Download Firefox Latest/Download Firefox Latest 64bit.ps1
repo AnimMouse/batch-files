a06a38a1b26fd02f53dd966bf9f83a92a3ecb40c
AnimMouse <git@animmouse.eu.org>
2020-12-19 00:06:53 +0800

    Add files

---
+++ Download Firefox Latest/Download Firefox Latest 64bit.ps1
@@ -0,0 +1,2 @@
+# This will download Mozilla Firefox en-US installer for Windows (64bit)
+Invoke-WebRequest "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US" -OutFile FirefoxSetup64.exe
