6cb834e868a692dbdf930051f586da3ceb65946f
AnimMouse <git@animmouse.eu.org>
2021-03-13 02:07:06 +0800

    Update encoder

--- FFmpeg Encoders/FFmpeg batch mkv to mp4.cmd
+++ FFmpeg Encoders/FFmpeg batch mkv to mp4.cmd
@@ -1,5 +1,6 @@
 @echo off
+:: Will encode opus to AAC
 setlocal
 :Start
-FOR /F "tokens=*" %%G IN ('dir /b *.mkv') DO ffmpeg -i "%%G" -c:v copy -c:a libfdk_aac -vbr:a 4 "%%~nG.mp4"
+FOR /F "tokens=*" %%G IN ('dir /b *.mkv') DO ffmpeg -hide_banner -i "%%G" -c:v copy -c:a libfdk_aac -vbr:a 4 "%%~nG.mp4"
 pause
